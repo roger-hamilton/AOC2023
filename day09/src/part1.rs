f6b1d579c835ce28838386c7325c45d16d8c7a15
Roger Hamilton <roger.hamilton@resultstack.com>
2023-12-09 11:45:42 -0600

    Refactor find_next/prev and simpified parse_input

--- day09/src/part1.rs
+++ day09/src/part1.rs
@@ -1,47 +1,40 @@
 use crate::Result;
 use itertools::Itertools;
-use nom::{
-    character::{
-        self,
-        complete::{line_ending, space1},
-    },
-    multi::separated_list1,
-    IResult,
-};
 
 #[derive(Debug)]
 pub struct Input {
     histories: Vec<Vec<i64>>,
 }
 
-fn parse_history(input: &str) -> IResult<&str, Vec<i64>> {
-    let (input, history) = separated_list1(
-        space1::<&str, nom::error::Error<&str>>,
-        character::complete::i64,
-    )(input)
-    .unwrap();
-    Ok((input, history))
-}
-
 pub fn parse_input(input: &str) -> Result<Input> {
-    let (_, histories) = separated_list1(line_ending, parse_history)(input).unwrap();
+    let histories = input
+        .lines()
+        .map(|l| {
+            l.split_ascii_whitespace()
+                .map(|n| n.parse::<i64>().unwrap())
+                .collect_vec()
+        })
+        .collect_vec();
 
     Ok(Input { histories })
 }
 
 fn find_next(history: &[i64]) -> i64 {
-    assert!(history.len() > 0);
     if history.len() == 1 {
         return history[0];
     }
 
-    let paired = history.iter().tuple_windows();
+    let diffs = history
+        .iter()
+        .tuple_windows()
+        .map(|(a, b)| b - a)
+        .collect_vec();
 
-    if paired.clone().all(|(a, b)| a == b) {
-        return history[0];
+    if diffs.iter().all(|a| a == &0) {
+        history[0]
+    } else {
+        history.last().unwrap() + find_next(&diffs)
     }
-
-    find_next(&paired.map(|(a, b)| b - a).collect_vec()) + history.iter().last().unwrap()
 }
 
 pub fn process(input: &str) -> Result<i64> {
--- day09/src/part2.rs
+++ day09/src/part2.rs
@@ -1,53 +1,46 @@
 use crate::Result;
 use itertools::Itertools;
-use nom::{
-    character::{
-        self,
-        complete::{line_ending, space1},
-    },
-    multi::separated_list1,
-    IResult,
-};
 
 #[derive(Debug)]
 pub struct Input {
     histories: Vec<Vec<i64>>,
 }
 
-fn parse_history(input: &str) -> IResult<&str, Vec<i64>> {
-    let (input, history) = separated_list1(
-        space1::<&str, nom::error::Error<&str>>,
-        character::complete::i64,
-    )(input)
-    .unwrap();
-    Ok((input, history))
-}
-
 pub fn parse_input(input: &str) -> Result<Input> {
-    let (_, histories) = separated_list1(line_ending, parse_history)(input).unwrap();
+    let histories = input
+        .lines()
+        .map(|l| {
+            l.split_ascii_whitespace()
+                .map(|n| n.parse::<i64>().unwrap())
+                .collect_vec()
+        })
+        .collect_vec();
 
     Ok(Input { histories })
 }
 
-fn find_next(history: &[i64]) -> i64 {
-    assert!(history.len() > 0);
+fn find_prev(history: &[i64]) -> i64 {
     if history.len() == 1 {
         return history[0];
     }
 
-    let paired = history.iter().tuple_windows();
+    let diffs = history
+        .iter()
+        .tuple_windows()
+        .map(|(a, b)| b - a)
+        .collect_vec();
 
-    if paired.clone().all(|(a, b)| a == b) {
-        return history[0];
+    if diffs.iter().all(|a| a == &0) {
+        history[0]
+    } else {
+        history[0] - find_prev(&diffs)
     }
-
-    history[0] - find_next(&paired.map(|(a, b)| b - a).collect_vec())
 }
 
 pub fn process(input: &str) -> Result<i64> {
     let input = parse_input(input)?;
 
-    Ok(input.histories.iter().map(|h| find_next(h)).sum())
+    Ok(input.histories.iter().map(|h| find_prev(h)).sum())
 }
 
 #[cfg(test)]
@@ -68,10 +61,10 @@ mod tests {
     }
 
     #[rstest]
-    #[case(vec![0, 3, 6, 9, 12, 15], -3)]
-    #[case(vec![1, 3, 6, 10, 15, 21], 0)]
-    #[case(vec![10, 13,16,21,30,45], 5)]
+    #[case(vec![ 0, 3,  6,  9,  12, 15], -3)]
+    #[case(vec![ 1, 3,  6,  10, 15, 21],  0)]
+    #[case(vec![10, 13, 16, 21, 30, 45],  5)]
     fn test_find_next(#[case] history: Vec<i64>, #[case] expected: i64) {
-        assert_eq!(expected, find_next(&history));
+        assert_eq!(expected, find_prev(&history));
     }
 }
